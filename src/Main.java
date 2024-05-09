095671c18b1a14d59b6d0f340384bb7249de46ea
pooja bharti <poojabharti80@gmail.com>
2024-05-10 00:01:37 +0300

    added a new line and update an existing line

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        System.out.println("this is my first commit");
+        System.out.println("this is my first commit-123");
+        System.out.println("this is my second commit");
 
     }
 }
